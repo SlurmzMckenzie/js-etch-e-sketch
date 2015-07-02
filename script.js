c6959ca987a9796fcc1d1f71799a13847a9db501
slurmzmckenzie <brendanwilde@gmail.com>
2015-07-02 16:48:40 +1000

    update event listener

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ document.getElementById('container').appendChild(div);
 
 }
 
-document.addEventListener("DOMContentLoaded",defaultDiv);
+document.addEventListener("DOMContentLoaded",defaultDiv,false);
 
 
 
-
+//console log to check if the event listener was done correctly
+//document.addEventListener("DOMContentLoaded", function(){console.log('I loaded from start')}, false);
 
