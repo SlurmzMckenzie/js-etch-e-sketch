cf5cdd60fdff0a52883292a3b361eeee03b06dd8
slurmzmckenzie <brendanwilde@gmail.com>
2015-07-07 07:14:10 +1000

    added grid container div, added if statement to check if it exists, if not do nothing, if so destroy it on the creation of a new grid.

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 
+var grid = 20;
+
+
+//draws new grid from prompt
+function newGrid() {
+	grid = prompt("How many squares would you like in your grid?");
+	defaultDiv();
+}
+
+//creates DIV element inside container
 function defaultDiv() {
-
-var div = document.createElement('DIV');
-
-document.getElementById('container').appendChild(div);
+	var i = 0;
+	while ( i < grid) {
+		var div = document.createElement('DIV');
+		document.getElementById('container').appendChild(div);
+		i++;
+	}
 
 }
 
+//creates div on page load
 document.addEventListener("DOMContentLoaded",defaultDiv,false);
 
 
 
-//console log to check if the event listener was done correctly
-//document.addEventListener("DOMContentLoaded", function(){console.log('I loaded from start')}, false);
+//This wrapper ensure that everything is run after the DOM is loaded
+window.addEventListener("load",function(){
 
+	//button listener event
+
+	var n = document.getElementById('newGridButton');
+	console.log(n);
+	n.addEventListener("click",newGrid,false);
+
+
+//End of paged loaded wrapper
+});
