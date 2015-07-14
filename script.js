815a909a860908f9cc941f785f2689643cf65fad
slurmzmckenzie <brendanwilde@gmail.com>
2015-07-14 16:36:10 +1000

    Added if function to validate that a grid is less than 1 million divs.

--- script.js
+++ script.js
@@ -1,26 +1,55 @@
 
-var grid = 20;
+// Default Grid Size
+var grid = Math.pow(16,2);
 
 
 //draws new grid from prompt
-function newGrid() {
-	grid = prompt("How many squares would you like in your grid?");
-	defaultDiv();
+function gridPrompt() {
+	grid = Math.pow(prompt("How many squares wide would you like your grid?"),2);
+	newGrid();
 }
 
 //creates DIV element inside container
-function defaultDiv() {
+function newGrid() {
+/*
+	If statement to check if the gridContainer DIV exists and if so destroy it
+	
+	if grid container exists destroy it
+
+	if doesn't exist do nothing
+
+*/
+	if (grid > 1000000) {
+		alert("Thats over 1 million squares, you'll regret it, enter a smaller number.");
+		gridPrompt();
+	};
+
+	var gridContainer = document.getElementById('gridContainer')
+
+	if (gridContainer != null) {
+		gridContainer.remove();
+	};
+
+// create a new gridContainer element
+
+
+	var div = document.createElement('DIV');
+	div.setAttribute("id", "gridContainer");
+	document.getElementById('container').appendChild(div); 
+
+
 	var i = 0;
 	while ( i < grid) {
-		var div = document.createElement('DIV');
-		document.getElementById('container').appendChild(div);
+		var gridDiv = document.createElement('DIV');
+		gridDiv.setAttribute("class", "gridsquare");
+		document.getElementById('gridContainer').appendChild(gridDiv); 
 		i++;
 	}
 
 }
 
 //creates div on page load
-document.addEventListener("DOMContentLoaded",defaultDiv,false);
+document.addEventListener("DOMContentLoaded",newGrid,false);
 
 
 
@@ -31,7 +60,7 @@ window.addEventListener("load",function(){
 
 	var n = document.getElementById('newGridButton');
 	console.log(n);
-	n.addEventListener("click",newGrid,false);
+	n.addEventListener("click",gridPrompt,false);
 
 
 //End of paged loaded wrapper
